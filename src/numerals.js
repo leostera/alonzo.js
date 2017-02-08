995c132a36c461c3c4b6ea425405da96a1692de5
Leandro Ostera <leandro@ostera.io>
2017-02-08 12:26:04 +0100

    Add predecessor

--- src/numerals.js
+++ src/numerals.js
@@ -1,12 +1,14 @@
 import {
   id,
   fst,
+  snd,
 } from './prelude'
 
 const zero = id
 const isZero = n => n(fst)
 
 const succ = n => s => s(False)(n)(zero)
+const pred = n => isZero(n)(zero)(n(snd))
 
 const one = succ(zero)
 const two = succ(one)
